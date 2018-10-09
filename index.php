d1950bca4ade6a63fa5e1624b40bf246198c625d
amirlavi2 <amir.lavi2@gmail.com>
2018-10-09 19:27:23 +0300

    database -> create a singleton

--- index.php
+++ index.php
@@ -1,4 +1,9 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', TRUE);
+ini_set('display_startup_errors', TRUE);
+
 require_once 'core/init.php';
 
-echo Config::get('mysql/host');
+
+DB::getInstance();
